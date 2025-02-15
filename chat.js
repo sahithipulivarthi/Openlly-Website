feb84aa618a4578525eee1a820a75b032dfa35be
Sahithi Pulivarthi <121199341+sahithipulivarthi@users.noreply.github.com>
2025-02-14 23:39:14 -0600

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,20 +1,26 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDK6SH0AHg91V6r32oJqND0HSwVVWJEFGo",
-  authDomain: "openlly-chat.firebaseapp.com",
-  projectId: "openlly-chat",
-  storageBucket: "openlly-chat.firebasestorage.app",
-  messagingSenderId: "688100647446",
-  appId: "1:688100647446:web:aa691dfc484ed712a2cb37",
-  measurementId: "G-WCPHMVTJ17"
-};
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDK6SH0AHg91V6r32oJqND0HSwVVWJEFGo",
+    authDomain: "openlly-chat.firebaseapp.com",
+    projectId: "openlly-chat",
+    storageBucket: "openlly-chat.firebasestorage.app",
+    messagingSenderId: "688100647446",
+    appId: "1:688100647446:web:aa691dfc484ed712a2cb37",
+    measurementId: "G-WCPHMVTJ17"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
 
 
 // Initialize Firebase
